de1ef42f7a7eb2eced9baca20c4953c3a60377b1
oumououattara <ouattaraoumou42@gmail.com>
2021-08-24 17:05:42 +0200

    product display

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -8,12 +8,21 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Class ProductController
+ * @package App\Controller
+ */
 class ProductController extends AbstractController
-{
+{   
+    /**
+     * Doctrine
+     *
+     * @var [type]
+     */
     private $entityManager;
 
     /**
-     * Undocumented function
+     * ProductController constructor.
      *
      * @param EntityManagerInterface $entityManager
      */
@@ -24,12 +33,31 @@ class ProductController extends AbstractController
 
     /**
      * @Route("/products", name="products")
+     *
+     * @return Response
      */
     public function index(): Response
     {
         $products = $this->entityManager->getRepository(Product::class)->findAll();
         return $this->render('product/index.html.twig', [
             'products' => $products
+        ]);
+    }
+
+    /**
+     * @Route("/product/{slug}", name="product")
+     *
+     * @param [type] $slug
+     * @return Response
+     */
+    public function show($slug): Response
+    {
+        $product = $this->entityManager->getRepository(Product::class)->findOneBy(['slug' => $slug]);
+        if (!$product) {
+            return $this->redirectToRoute('products');
+        }
+        return $this->render('product/show.html.twig', [
+            'product' => $product
         ]);
     }
 }
