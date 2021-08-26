679a40a8f4835637c28dd3bf3ba0667b587ca503
oumououattara <ouattaraoumou42@gmail.com>
2021-08-26 12:54:09 +0200

    start of the cart 1/3

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Classe\Cart;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,9 +15,65 @@ class CartController extends AbstractController
 {
     /**
      * @Route("/cart", name="cart")
+     *
+     * @param Cart $cart
+     * @return Response
      */
-    public function index(): Response
+    public function index(Cart $cart): Response
     {
-        return $this->render('cart/index.html.twig');
+        return $this->render('cart/index.html.twig', [
+            'cart' => $cart->getFullCart()
+        ]);
+    }
+
+    /**
+     * @Route("/cart/add/{id}", name="cart_add")
+     *
+     * @param Cart $cart
+     * @param [type] $id
+     * @return Response
+     */
+    public function add(Cart $cart, $id): Response
+    {
+        $cart->add($id);
+        return $this->redirectToRoute('cart');
+    }
+
+    /**
+     * @Route("/cart/decrease/{id}", name="cart_decrease")
+     *
+     * @param Cart $cart
+     * @param [type] $id
+     * @return Response
+     */
+    public function decrease(Cart $cart, $id): Response
+    {
+        $cart->decrease($id);
+        return $this->redirectToRoute('cart');
+    }
+
+    /**
+     * @Route("/cart/remove", name="cart_remove")
+     *
+     * @param Cart $cart
+     * @return Response
+     */
+    public function remove(Cart $cart): Response
+    {
+        $cart->remove();
+        return $this->redirectToRoute('products');
+    }
+
+    /**
+     * @Route("/cart/delete/{id}", name="cart_delete")
+     *
+     * @param Cart $cart
+     * @param [type] $id
+     * @return Response
+     */
+    public function delete(Cart $cart, $id): Response
+    {
+        $cart->delete($id);
+        return $this->redirectToRoute('cart');
     }
 }
